394380898958e00f9720ec402c1477afdd40e2d4
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-04-02 22:12:55 -0600

    Added Jquery and fixed my log consoles

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -10,8 +10,8 @@ var searchBtn=document.querySelector('.search');
 
 
 // Fetch the geolocation
-function getGeoLocation(){
-var geoLocationAPI="http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=1&appid=8052fe1a5f6b6e4f4d138051e243f44e";
+function getGeoLocation(city){
+var geoLocationAPI="http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=8052fe1a5f6b6e4f4d138051e243f44e";
 fetch(geoLocationAPI)
 .then(function(response){
 // console.log(response);
@@ -23,13 +23,13 @@ return response.json()
     // passing the value from function getGeoLocation into function getWeatherInfo
     getWeatherInfo(data[0].lat,data[0].lon);
 })};
-getGeoLocation();
 
 // Fetch the weather information
 function getWeatherInfo(latitude,longitude){
     console.log(latitude,longitude);
     var weatherAPI="http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=8052fe1a5f6b6e4f4d138051e243f44e";
-fetch(weatherAPI)
+    console.log(weatherAPI);
+    fetch(weatherAPI)
     .then(function(response){
         return response.json();
     })
@@ -39,9 +39,10 @@ fetch(weatherAPI)
 }
 
 // Save text value of search and save it into local storage within the Array
-searchBtn.addEventListener('click', function(getGeoLocation){
+searchBtn.addEventListener('click', function(event){
     var city=document.querySelector('#city');
     var savedCity=document.querySelector('.textVal').value;
+    getGeoLocation(city.value);
     city.textContent=savedCity;
     if (city===""){
         return;
@@ -53,27 +54,26 @@ searchBtn.addEventListener('click', function(getGeoLocation){
 })
 
 // Create buttons from saved text value that also acts as a search button based on history search
-var savedHist=document.querySelector('.city-history');
+var cityHistEl=$('.city-history');
 function savedHistory(){
+    var savedHist=localStorage.getItem("city");
     for (i=0; i<cityHistArr.length; i++){
-        var rowEl=document.querySelector("row");
-        var btnEl=document.querySelector("button");
-        btnEl.textContent=savedHist[i];
-        rowEl.classList.add('histBtnRow');
-        btnEl.classList.add('histBtn');
-        btnEl.setAttribute('type', 'button');
+    //     var rowEl=document.getElementsByName("row");
+    //     var btnEl=document.getElementsByName("button");
+    //     btnEl.textContent=savedHist[i];
 
-        savedHist.prepend(rowEl);
-        rowEl.append(btnEl);        
-    } if (!city){
-        return;
+    //     rowEl.classList.add('histBtnRow');
+    //     btnEl.classList.add('btn btn-outline-secondary histBtn');
+    //     btnEl.setAttribute('type', 'button');
+
+    //     savedHist.prepend(rowEl);
+    //     rowEl.append(btnEl);        
+    // } if (!city){
+    //     return;
+    // }
+    var button=$("<button>");
+    button.text(savedHist[i]);
+    button.on('click', getWeatherInfo);
+    button.append(cityHistEl);
     }
-
-// Allows the button to be used as a search from history
-var histBtn=document.querySelector('.histBtnRow');
-histBtn.addEventListener('click', function(event){
-    console.log('saved history list');
-    event.preventDefault();
-
-}) 
 }
